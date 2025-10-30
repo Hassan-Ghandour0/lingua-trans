7a811f63ad532bc9628e54c67439c92a34b23ad3
Hassan Ghandour <hassanghandour889@gmail.com>
2025-10-30 12:59:37 +0100

    chore: initial backend & frontend

---
+++ backend/src/test/java/com/lingua/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingua.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
