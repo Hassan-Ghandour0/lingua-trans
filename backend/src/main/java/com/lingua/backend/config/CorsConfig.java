f6955029ec3bb552eeb377b3739e4adaa76bbe10
Hassan Ghandour <hassanghandour889@gmail.com>
2025-11-06 20:41:37 +0100

    update corsConfig

--- backend/src/main/java/com/lingua/backend/config/CorsConfig.java
+++ backend/src/main/java/com/lingua/backend/config/CorsConfig.java
@@ -15,10 +15,16 @@ public class CorsConfig {
   public CorsFilter corsFilter() {
     CorsConfiguration cfg = new CorsConfiguration();
     cfg.setAllowCredentials(true);
-    cfg.setAllowedOrigins(List.of("http://localhost:4200"));
-    cfg.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+    
+    
+    cfg.setAllowedOrigins(List.of(
+        "http://localhost:4200",
+        "https://ipa-tr.onrender.com"   
+    ));
+    
+    cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     cfg.setAllowedHeaders(List.of("*"));
-
+    
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", cfg);
     return new CorsFilter(source);
